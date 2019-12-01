4d5c8941bcfeda673809e4c960ee8db4dfc702a8
OvidiuTodea <50288239+OvidiuTodea@users.noreply.github.com>
2019-12-01 18:28:28 +0200

    initial commit
    first java programme

---
+++ Hello1/src/com/company/Hello1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Hello1 {
+
+    public static void main(String[] args) {
+
+        // java -version  -->vedem versiunea de java folosita in cmd
+        /*
+         in CMD :
+         1. set Path=C:\Program Files\Java\jdk1.8.0_231\bin;
+         2. javac filename.java  -->compile ;
+         3. java filename  -->run
+        */
+        System.out.println("Hello, Cluj :) !");
+        System.out.println("La multi ani, Romania! ");
+    }
+}
